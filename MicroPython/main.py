8c7734477f3663331c822d76422c97d68f3cd1b8
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-03-02 17:49:01 +0000

    conrols RGP LED using a latch

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,24 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Dat Nguyen
+Created on: Feb 2026
+This program uses an SR latch to control an RGB LED.
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    pin15.write_digital(0)
+    pin16.write_digital(0)
+    if button_a.is_pressed() and button_b.is_pressed():
+        display.show("A+B")
+        pin15.write_digital(1)
+        pin16.write_digital(1)
+    elif button_a.is_pressed():
+        display.show("A")
+        pin15.write_digital(1)
+    elif button_b.is_pressed():
+        display.show("B")
+        pin16.write_digital(1)
