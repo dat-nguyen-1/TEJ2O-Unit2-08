a58182b78fbf61318ab3b9d87b8eefaf748e086e
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-03-06 09:05:29 -0500

    Refactor comments for LED color changes

--- main.ts
+++ main.ts
@@ -5,82 +5,68 @@
  * This program will change the colour of an RGB LED.
 */
 
-// Initialize display.
+// Initialize display
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// Initialize pins.
-pins.digitalWritePin(DigitalPin.P14, 0) // Red.
-pins.digitalWritePin(DigitalPin.P15, 0) // Green.
-pins.digitalWritePin(DigitalPin.P16, 0 )// Blue.
+// Initialize pins to off
+pins.digitalWritePin(DigitalPin.P14, 0) // Red
+pins.digitalWritePin(DigitalPin.P15, 0) // Green
+pins.digitalWritePin(DigitalPin.P16, 0) // Blue
 
 input.onButtonPressed(Button.A, function() {
-    // Turn LED red for 1 second.
+    // Red
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.showString("Red")
     basic.pause(1000)
-
-    // Turn LEDs off.
     pins.digitalWritePin(DigitalPin.P14, 0)
 
-    // Turn LED green for 1 second.
+    // Green
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.showString("Green")
     basic.pause(1000)
-
-    // Turn LEDs off.
     pins.digitalWritePin(DigitalPin.P15, 0)
 
-    // Turn LED blue for 1 second.
+    // Blue
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.showString("Blue")
     basic.pause(1000)
-
-    // Turn LEDs off.
     pins.digitalWritePin(DigitalPin.P16, 0)
 
-    // Turn LED magenta for 1 second.
+    // Magenta (Red + Blue)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.showString("Magenta")
     basic.pause(1000)
-
-    // Turn LEDs off.
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P16, 0)
 
-    // Turn LED cyan for 1 second.
+    // Cyan (Green + Blue)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.showString("Cyan")
     basic.pause(1000)
-
-    // Turn LEDs off.
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P16, 0)
 
-    // Turn LED yellow for 1 second.
+    // Yellow (Red + Green)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.showString("Yellow")
     basic.pause(1000)
-
-    // Turn LEDs off.
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
 
-    // Turn LED white for 1 second.
+    // White (Red + Green + Blue)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.showString("White")
     basic.pause(1000)
-
-    // Turn LEDs off.
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P16, 0)
 
-    // Display happy face.
+    // Display happy face
     basic.showIcon(IconNames.Happy)
 })
